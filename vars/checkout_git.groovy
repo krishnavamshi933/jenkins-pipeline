c775e5f8023e22e7fdde6dc662c661f422d1fb01
krishnavamshi933 <ckrishnavamshi595@gmail.com>
2022-07-21 18:53:38 +0530

    hfg

--- vars/checkout_git.groovy
+++ vars/checkout_git.groovy
@@ -1,7 +1,7 @@
 def checkout_git(String repo String branch) {
     
     echo "This is for checking out git code."
-   git credentialsId: 'krishnavamshi933', url: "git@github.com:krishnavamshi933/${repo}.git", branch: "master"
+   git branch: 'main', credentialsId: 'krishnavamshi933', url: "git@github.com:krishnavamshi933/"${repo}".git", branch: "master"
 
 }
 
