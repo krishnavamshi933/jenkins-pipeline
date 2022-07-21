91470d67bdc124d1ec359cc5a761b912b37e99c5
krishnavamshi933 <ckrishnavamshi595@gmail.com>
2022-07-21 18:29:45 +0530

    saed

--- vars/checkout_git.groovy
+++ vars/checkout_git.groovy
@@ -1,7 +1,7 @@
 def checkout_git() {
     
     echo "This is for checking out git code."
-   git credentialsId: 'krishnavamshi933', url: "git@github.com:krishnavamshi933/java-hello-world-with-maven.git"
+   git credentialsId: 'krishnavamshi933', url: "git@github.com:krishnavamshi933/${}.git"
 
 }
 
